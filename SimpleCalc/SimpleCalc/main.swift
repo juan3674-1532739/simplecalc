d59867dd8bf89c8a7cbc6991d347b8c688b436ea
juan3674 <juan3674@uw.edu>
2019-01-15 16:31:21 -0800

    finished hw

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,49 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var op : String = ""
+        if Int(args[args.count - 1]) == nil {
+            op = args[args.count - 1]
+        } else {
+            op = args[args.count - 2]
+        }
+        switch op {
+        case "+":
+            return Int(args[0])! + Int(args[2])!
+        case "-":
+            return Int(args[0])! - Int(args[2])!
+        case "*":
+            return Int(args[0])! * Int(args[2])!
+        case "/":
+            return Int(args[0])! / Int(args[2])!
+        case "%":
+            return Int(args[0])! % Int(args[2])!
+        case "count":
+            if args.count > 1 {
+                return args.count - 1
+            }
+        case "avg":
+            if args.count > 1 {
+                var sum = 0;
+                for index in 0..<args.count - 1 {
+                    sum += Int(args[index])!
+                }
+                return sum / (args.count - 1)
+            }
+        case "fact":
+            if args[0] == "1" || args[0] == "0" {
+                return 1
+            } else if args.count > 1 {
+                var fact = 1
+                for x in 1...Int(args[0])! {
+                    fact *= x
+                }
+                return fact
+            }
+        default:
+            print("Unexpected error occured")
+        }
+        return 0
     }
     
     public func calculate(_ arg: String) -> Int {
